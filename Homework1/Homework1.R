bfb065aa7e98cd1a817fb7f8d0fa5e08e9c67db7
Daniel823 <dglownia222@gmail.com>
2016-09-28 18:42:54 -0500

    add homework document

---
+++ Homework1/Homework1.R
@@ -0,0 +1,45 @@
+#Problem 1 (Intel Dataset)
+#a. 
+
+#b.
+
+#c.
+
+#d.
+
+
+#Problem 2 (experiment.xls)
+
+#a. 
+
+#b.
+
+#c.
+
+#d.
+
+#e. 
+
+
+#Problem 3
+#a. 
+
+#b.
+
+#c.
+
+
+#Problem 4 
+#a. 
+
+#b.
+
+
+#Problem 5
+#a. 
+
+#b.
+
+#c
+
+
