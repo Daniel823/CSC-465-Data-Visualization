ac34945b076204edce78c2fe402f9b7114ae4399
Daniel823 <dglownia222@gmail.com>
2016-10-01 12:00:41 -0500

    Add working portion of problem a

--- Homework1/Homework1.R
+++ Homework1/Homework1.R
@@ -1,6 +1,13 @@
 #Problem 1 (Intel Dataset)
 #a. 
-heisenberg <- read.csv(file="C:/Users/dglownia/Documents/GitHub/CSC-465-Data-Visualization/Homework1/Datasets/Intel-1998.csv",head=TRUE,sep=",")
+intel <- read.csv(file="C:/Users/dglownia/Documents/GitHub/CSC-465-Data-Visualization/Homework1/Datasets/Intel-1998.csv",head=TRUE,sep=",")
 
 
+ggplot(intel, aes(Close, Date)) + geom_line() + theme_bw()
 
+ggplot(data=intel, aes(x=Close, y=Date, group=1)) +
+  geom_line() +
+  geom_point() +
+  expand_limits(y=0) +
+  xlab("Close") + ylab("Date") +
+  ggtitle("closing price vs. the date")
