ac289c9291c4167b4fec9a6f5dcf87d00e0cf128
Daniel823 <dglownia222@gmail.com>
2016-10-02 17:34:32 -0500

    Add problems 2 and 3

--- Homework1/Homework1.R
+++ Homework1/Homework1.R
@@ -3,11 +3,7 @@
 intel <- read.csv(file="C:/Users/dglownia/Documents/GitHub/CSC-465-Data-Visualization/Homework1/Datasets/Intel-1998.csv",head=TRUE,sep=",")
 
 
-ggplot(intel, aes(Close, Date)) + geom_line() + theme_bw()
-
-ggplot(data=intel, aes(x=Close, y=Date, group=1)) +
-  geom_line() +
-  geom_point() +
-  expand_limits(y=0) +
-  xlab("Close") + ylab("Date") +
-  ggtitle("closing price vs. the date")
+experiment[, "stdErrorCategory"] <- NA
+experiment$stdErrorCategory <- ifelse(experiment$Error > 0.5,"A", "NAN")
+experiment$stdErrorCategory <- ifelse(experiment$Error < -0.5,"C", "NAN")
+experiment$stdErrorCategory <- ifelse(experiment$Error >= -0.5 || experiment$Error <= 0.5 ,"B", "NAN")
