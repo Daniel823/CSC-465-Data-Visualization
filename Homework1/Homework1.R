69153d0fc6a55efcd343d5dabdfa882834f22e63
Daniel823 <dglownia222@gmail.com>
2016-10-03 08:11:11 -0500

    Add Homework PDF

--- Homework1/Homework1.R
+++ Homework1/Homework1.R
@@ -2,8 +2,10 @@
 #a. 
 intel <- read.csv(file="C:/Users/dglownia/Documents/GitHub/CSC-465-Data-Visualization/Homework1/Datasets/Intel-1998.csv",head=TRUE,sep=",")
 
+experiment[, "absError"] <- NA
+experiment$absError <- abs(Error)
 
-experiment[, "stdErrorCategory"] <- NA
-experiment$stdErrorCategory <- ifelse(experiment$Error > 0.5,"A", "NAN")
-experiment$stdErrorCategory <- ifelse(experiment$Error < -0.5,"C", "NAN")
-experiment$stdErrorCategory <- ifelse(experiment$Error >= -0.5 || experiment$Error <= 0.5 ,"B", "NAN")
+ggplot(data=experiment, aes(x=Trial, y=absError)) + 
+  geom_bar(colour="black", fill="#DD8888", width=.8, stat="identity") + 
+  xlab("Trail") + ylab("Error") +
+  ggtitle("Median Errors by Test")
